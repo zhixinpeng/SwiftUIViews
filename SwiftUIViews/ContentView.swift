2688235a1a8befbf2254cf32518a82abc7fa29b1
Angus <pengzhixin@live.cn>
2021-03-30 21:12:06 +0800

    feat: initial commit

---
+++ SwiftUIViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIViews
+//
+//  Created by 彭智鑫 on 2021/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIViews/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  SwiftUIViews
+//
+//  Created by 彭智鑫 on 2021/3/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
